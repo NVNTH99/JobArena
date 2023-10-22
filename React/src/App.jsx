d5d3f6628be58bc1c461ca7d944ced0282cd1012
Gigil James <70816318+gigiljames@users.noreply.github.com>
2023-10-22 14:00:49 +0530

    error_solved

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';  
 import NoPage from './Pages/NoPage';
 import Login from './Pages/Login';
+import JobApplication from './Pages/JobApplication';
+import CandidateProfle from "./Pages/CandidateProfile";
 
 function App() {
   // const history = useHistory();
