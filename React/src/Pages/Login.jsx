79a611c3b81c5fc2a85a9ec4622b2132b46d929f
NVNTH99 <navaneethshanavasan02@gmail.com>
2023-10-22 14:14:48 +0530

    Login page changes

--- React/src/Pages/Login.jsx
+++ React/src/Pages/Login.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React,{ useState } from "react";
 import './Login.css'
+// import { response } from "express";
+import axios from 'axios'
+// import { useHistory } from 'react-router-dom';
 
 function Login(){
+    const [credentials, setcred] = useState({
+        username:'',
+        password: ''
+    })
+    // const history = useHistory();
+    const [retrieved , setretrieval] = useState('')
+    const [error, seterror] = useState('')
+
+    const loginfunc = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:3000/login',credentials)
+        .then(response => {
+            if(typeof response.data === 'string'){
+                setretrieval(response.data)
+                if(retrieved === 'candidate'){
+                    // history.push()
+                }
+                else{
+
+                }
+            }
+            else{
+                setcred({
+                    username:'',
+                    password:''
+                })
+                seterror('Invalid login credentials')
+            }
+        })
+        .catch(error => {
+            console.log("Error while logging in")
+        })
+    }
+
     return(
         <>
             <div className="container">
@@ -10,14 +47,42 @@ function Login(){
                         <form className="login">
                             <div className="login__field">
                                 <i className="login__icon fa fa-solid fa-user"></i>
-                                <input type="text" className="login__input" placeholder="Username"></input>
+                                <input 
+                                name="username"
+                                nametype="text" 
+                                className="login__input" 
+                                placeholder="Username"
+                                value={credentials.username}
+                                onChange={(e)=>{
+                                    const {name,value} = e.target
+                                    setcred((prevCredentials)=>({
+                                        ...prevCredentials,
+                                        [name]:value
+                                    }))
+                                }}
+                                >
+                                </input>
                             </div>
                             <div className="login__field">
                                 <i className="login__icon fa fa-lock"></i>
-                                <input type="password" className="login__input" placeholder="Password"></input>
+                                <input 
+                                name="password" 
+                                type="password" 
+                                className="login__input" 
+                                placeholder="Password" 
+                                value={credentials.password} 
+                                onChange={(e)=>{
+                                    const {name,value} = e.target
+                                    setcred((prevCredentials)=>({
+                                        ...prevCredentials,
+                                        [name]:value
+                                    }))
+                                }}>
+
+                                </input>
                             </div>
                             <button className="button">
-                                <a href="/"><button className="bn30">Login</button></a>
+                                <button className="bn30" onClick={loginfunc}>Login</button>
                             </button>
                         </form>
                         <div className="guestbtn extra-link">
