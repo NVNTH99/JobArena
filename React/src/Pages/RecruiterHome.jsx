7c3e720a40ecb1f911576fbe626c0a0f82d36392
aadhi-prem <aditya.premjit@gmail.com>
2023-10-24 12:14:59 +0530

    recruiterhome

---
+++ React/src/Pages/RecruiterHome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import TabGroup from "../components/TabGroup";
+import "./RecruiterHome.css";
+import UpcomingCard from "../components/UpcomingCard";
+
+function RecruiterHome(){
+    return <>
+    </>
+}
