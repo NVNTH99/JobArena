f2ae1727c69c3bc1d1ee45cd5986f435163f7b3b
AmalMani <amal_b200761cs@nitc.ac.in>
2023-10-28 19:58:34 +0530

    rec add job, cand profile

--- React/src/Pages/RecruiterAddJob.jsx
+++ React/src/Pages/RecruiterAddJob.jsx
@@ -11,16 +11,109 @@ var upcoming = [
 ]
 var events = {"upcoming":upcoming};
 
-function RecruiterAddJob(){
+function RecruiterAddJob(){ 
     return(
         <>
             <Navbar userType = "recruiter"/>
             <Heading title = "Add Job"/>
-            <div>
-                <div>
+            <div classNameName="add-job-body">
+                <div classNameName="add-job-aditya">
+                    <div className="alllll">
+                        <section className="lhs">
+                            <form id="addjf">
+                                <div className="addjobbg">
+                                    <div className="pettiaj">
+                                        <div className="aj__field">
+                                            <label htmlFor="jobtitle" className="lbl">Job Title</label>
+                                            <input type="text" className="aj__input" id="jobtitle" aria-describedby="jobtitle"></input>
+                                        </div>
+                                        <div className="aj__field">
+                                            <label htmlFor="companyname" className="lbl">Company Name</label>
+                                            <input type="text" className="aj__input" id="companyname" placeholder="Company Name" disabled></input>
+                                        </div>
+                                    </div>
+                                    <div className="pettiaj">
+                                        <div className="aj__field">
+                                            <label htmlFor="JobDescription" className="lbl">Job Description</label>
+                                            <textarea className="aj__input" id="JobDescription" rows="8"></textarea>
+                                        </div>
+                                        <div className="aj__field">
+                                            <label htmlFor="domain" className="lbl">Required Domain</label>
+                                            <div className="catgrpaj">
+                                                <input type="text" id="domainfieldaj" className="aj__input" 
+                                                    aria-describedby="domain"></input>
+                                                <button type="button" id="domainplusaj">ADD&nbsp;<i className="fa fa-plus"></i></button>
+                                            </div>
+                                            <div id="domain-listaj"></div>
+                                        </div>
 
+                                    </div>
+                                    <div className="pettiaj">
+                                        <div className="aj__field">
+                                            <label htmlFor="location" className="lbl">Location</label>
+                                            <input type="text" className="aj__input" id="location"></input>
+                                        </div>
+                                        <div className="aj__field">
+                                            <label htmlFor="salary" className="lbl">Salary per annum</label>
+                                            <input type="number" className="aj__input" id="salary"></input>
+                                        </div>
+                                    </div>
+                                    <div className="pettiaj">
+                                        <div className="aj__field">
+                                            <label htmlFor="workdays" className="lbl">Work Days</label>
+                                            <input type="text" className="aj__input" id="workdays" aria-describedby="workdays"></input>
+
+                                        </div>
+                                        <div className="aj__field">
+                                            <label htmlFor="salary" className="lbl">Work Hours</label>
+                                            <input type="text" className="aj__input" id="salary" placeholder="[Start Time] - [End Time]"></input>
+                                        </div>
+                                    </div>
+                                    <div className="pettiaj">
+                                        <div className="aj__field">
+                                            <label htmlFor="responsibility" className="lbl">Responsibility</label>
+                                            <textarea className="aj__input" id="responsibility" rows="5"></textarea>
+                                        </div>
+                                        <div className="aj__field">
+                                            <label htmlFor="requirements" className="lbl">Requirements</label>
+                                            <textarea className="aj__input" id="requirements" rows="5"></textarea>
+                                        </div>
+                                    </div>
+                                    <div className="pettiaj">
+                                        <div className="aj__field">
+                                            <label htmlFor="disability" className="lbl">Job Type</label>
+                                            <div className="button-container-aj">
+                                                <button type="button" className="buttongaj" id="onsite">Onsite</button>
+                                                <button type="button" className="buttongaj" id="wfh">Work from Home</button>
+                                                <button type="button" className="buttongaj" id="hybrid">Hybrid</button>
+                                            </div>
+                                        </div>
+                                        <div className="aj__field">
+                                            <label htmlFor="deadline" className="lbl">Deadline</label>
+                                            <div className="input-container">
+                                                <input type="date" className="aj__input" id="ajdate" name="deadline"></input>
+                                                <input type="time" className="aj__input" id="ajtime" name="time"></input>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="formsavebtn">
+                                        <a className="savebtn" href="#">
+                                            <p>Save</p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                            </form>
+                            <div className="screen__backgroundaj">
+                                <span className="screen__background__shapeaj screen__background__shape4aj"></span>
+                                <span className="screen__background__shapeaj screen__background__shape3aj"></span>
+                                <span className="screen__background__shapeaj screen__background__shape2aj"></span>
+                                <span className="screen__background__shapeaj screen__background__shape1aj"></span>
+                            </div>
+                        </section>
+                    </div>
                 </div>
-                <UpcomingCard event = {events}/>
+                {/* <UpcomingCard event = {events.upcoming}/> */}
             </div>
         </>
     )
--- React/src/Pages/cand_profile.jsx
+++ React/src/Pages/cand_profile.jsx
@@ -1,82 +1,187 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Heading from "../components/Heading";
+import UpcomingCard from "../components/UpcomingCard";
 import "./cand_profile.css"
 // import 'bootstrap/dist/css/bootstrap.min.css'
-function Cand_profile(){
-    return(
-        <>
-        <Navbar userType="candidate"/>
-        <div className="alllll">
-            <h1 id="yourjobs">Profile</h1>
-            <hr id="hrr"></hr>
-            
-            <section className="lhs">
-                <form>
-                    <div className="row g-3">
-                        <div className="col-6">
-                            <label htmlFor="firstname" className="form-label">First Name</label>
-                            <input type="text" className="form-control" id="firstname" aria-describedby="first-name"></input>
-                        </div>
-                        <div className="col-6">
-                            <label htmlFor="lastname" className="form-label">Last Name</label>
-                            <input type="text" className="form-control" id="lastname"></input>
-                        </div>
-                        <div className="col-md-6">
-                            <label htmlFor="linkedin" className="form-label">LinkedIn Profile</label>
-                            <input type="text" className="form-control" id="linkedin"></input>
-                        </div>
-                        <div className="col-md-4">
-                            <label htmlFor="dob" className="form-label">Date Of Birth</label>
-                            <input type="text" className="form-control" id="dob"></input>
-                        </div>
-                        <div className="col-md-2">
-                            <label htmlFor="gender" className="form-label">Gender</label>
-                            <select id="gender" className="form-select" defaultValue={'male'}>
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                                <option value="trans">Transgender</option>
-                            </select>
-                        </div>
-                        <div className="col-6">
-                            <label htmlFor="Phno" className="form-label">Phone Number</label>
-                            <input type="number" className="form-control" id="Phno"></input>
-                        </div>
-                        <div className="col-6">
-                            <label htmlFor="nationality" className="form-label">Nationality</label>
-                            <input type="text" className="form-control" id="nationality"></input>
-                        </div>
-                        <div className="col-6">
-                            <label htmlFor="address" className="form-label">Address</label>
-                            <textarea className="form-control" id="address" rows="4"></textarea>
-                        </div>
-                        <div className="col-6">
-                            <label htmlFor="skills" className="form-label">Skills</label>
-                            <textarea className="form-control" id="skills" rows="4"></textarea>
-                        </div>
-                        <div className="col-6">
-                            <label htmlFor="resume" className="form-label">Upload Resume</label>
-                            <input type="file" className="form-control" id="resume" aria-describedby="resume"></input>
-                        </div>
-                        <div className="col-6">
-                            <label htmlFor="languages" className="form-label">Languages</label>
-                            <input type="text" className="form-control" id="languages" aria-describedby="languages"></input>
-                        </div>
-                        <div className="col-6">
-                            <label htmlFor="disability" className="form-label">Disability</label>
-                            <input type="text" className="form-control" id="disability" aria-describedby="disability"></input>
-                        </div>
-                        <div className="col-3"></div>
-                        <div className="col-3" id="formsavebtn">
-                            <a className="savebtn" href="#">
-                                <p>Save</p>
-                            </a>
-                        </div>
+
+var upcoming = [
+    {title:"Job Title", name:"Candidate Name", id:"#CandidateID" , date:"dd/mm/yyyy", time:"hh:mm"},
+    {title:"Job Title", name:"Candidate Name", id:"#CandidateID" , date:"dd/mm/yyyy", time:"hh:mm"},
+    {title:"Job Title", name:"Candidate Name", id:"#CandidateID" , date:"dd/mm/yyyy", time:"hh:mm"},
+]
+var events = {"upcoming":upcoming};
+
+function Cand_profile() {
+  return (
+    <>
+    <Navbar userType = "candidate"/>
+    <Heading title = "Candidate Profile"/>
+    <div>
+        <div className="alllllcp">
+            <section className="lhscp">
+                <form id="addcp">
+                <div className="cpbg">
+                    <div className="petticp">
+                    <div className="cp__field">
+                        <label htmlFor="firstname" className="lblcp">First Name</label>
+                        <input type="text" className="cp__input" id="firstname" aria-describedby="firstname" />
                     </div>
+                    <div className="cp__field">
+                        <label htmlFor="lastname" className="lblcp">Last Name</label>
+                        <input type="text" className="cp__input" id="lastname" />
+                    </div>
+                    </div>
+                    <div className="petticp">
+                    <div className="cp__field">
+                        <label htmlFor="linkedin" className="lblcp">LinkedIn Profile</label>
+                        <input type="text" className="cp__input" id="linkedin" />
+                    </div>
+                    <div className="cp__field">
+                        <label htmlFor="dob" className="lblcp">Date Of Birth</label>
+                        <input type="date" className="cp__input" id="dob" />
+                    </div>
+                    </div>
+                    <div className="petticp">
+                    <div className="cp__field">
+                        <label htmlFor="Phno" className="lblcp">Phone Number</label>
+                        <input type="number" className="cp__input" id="Phno" />
+                    </div>
+                    <div className="cp__field">
+                        <label htmlFor="nationality" className="lblcp">Nationality</label>
+                        <input type="text" className="cp__input" id="nationality" />
+                    </div>
+                    </div>
+                    <div className="petticp">
+                    <div className="cp__field">
+                        <label htmlFor="gender" className="lblcp">Gender</label>
+                        <select id="gender" className="dropdowncp">
+                        <option selected value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="trans">Transgender</option>
+                        </select>
+                    </div>
+                    <div className="cp__field">
+                        <label htmlFor="disability" className="lblcp">Disability</label>
+                        <input type="text" className="cp__input" id="disability" aria-describedby="disability" />
+                    </div>
+                    </div>
+                    <div className="petticp">
+                    <div className="cp__field">
+                        <label htmlFor="address" className="lblcp">Address</label>
+                        <textarea className="cp__input" id="address" rows="4"></textarea>
+                    </div>
+                    <div className="cp__field">
+                        <label htmlFor="skills" className="lblcp">Skills</label>
+                        <textarea className="cp__input" id="skills" rows="4"></textarea>
+                    </div>
+                    </div>
+                    <div className="petticp">
+                    <div className="cp__field">
+                        <label htmlFor="resume" className="lblcp">Upload Resume</label>
+                        <input type="file" className="cp__input" id="resume" aria-describedby="resume" />
+                    </div>
+                    <div className="cp__field"></div>
+                    </div>
+                    <div className="petticp">
+                    <div className="cp__field">
+                        <label htmlFor="languages" className="lblcp">Languages</label>
+                        <div className="catgrpcp">
+                        <input type="text" id="langfieldcp" className="cp__input" aria-describedby="languages" />
+                        <button type="button" id="langpluscp">ADD <i className="fa fa-plus"></i></button>
+                        </div>
+                        <div id="lang-listcp"></div>
+                    </div>
+                    <div className="cp__field">
+                        <label htmlFor="domain" className="lblcp">Interested Domain</label>
+                        <div className="catgrpcp">
+                        <input type="text" id="domainfieldcp" className="cp__input" aria-describedby="domain" />
+                        <button type="button" id="domainpluscp">ADD <i className="fa fa-plus"></i></button>
+                        </div>
+                        <div id="domain-listcp"></div>
+                    </div>
+                    </div>
+                    <div className="xtra-formcp">
+                    <h2 className="xtra-headingcp">Work Experience</h2>
+                    <hr className="hrrcp" />
+                    <div className="xtra-form-container">
+                        <div className="petticp">
+                        <div className="cp__field">
+                            <label htmlFor="jobtitle" className="lblcp">Job Title</label>
+                            <input type="text" className="cp__input" id="jobtitle" aria-describedby="jobtitle" />
+                        </div>
+                        <div className="cp__field">
+                            <label htmlFor="orgname" className="lblcp">Organisation Name</label>
+                            <input type="text" className="cp__input" id="orgname" />
+                        </div>
+                        </div>
+                        <div className="petticp">
+                        <div className="cp__field">
+                            <label htmlFor="startyear" className="lblcp">Start Year</label>
+                            <input type="number" className="cp__input" id="startyear" aria-describedby="startyear" />
+                        </div>
+                        <div className="cp__field">
+                            <label htmlFor="endyear" className="lblcp">End Year</label>
+                            <input type="text" className="cp__input" id="endyear" />
+                        </div>
+                        </div>
+                        <div className="petticp-btn">
+                        <button type="button" className="delwkcp" id="deleteButton1" value="Delete">Delete</button>
+                        </div>
+                        <hr className="smhrr" />
+                    </div>
+                    <div className="petticp-btn" id="xxx3">
+                        <button type="button" className="addwkcp" id="addMoreButton" value="addmore">Add More</button>
+                    </div>
+                    </div>
+                    <div className="xtra-formcp2">
+                    <h2 className="xtra-headingcp">Projects</h2>
+                    <hr className="hrrcp" />
+                    <div className="xtra-form-container2">
+                        <div className="petticp">
+                        <div className="cp__field">
+                            <label htmlFor="projecttitle" className="lblcp">Project Title</label>
+                            <input type="text" className="cp__input i1" id="projecttitle" aria-describedby="projecttitle" />
+                        </div>
+                        <div className="cp__field"></div>
+                        </div>
+                        <div className="petticp">
+                        <div className="cp__field">
+                            <label htmlFor="startyear2" className="lblcp">Start Year</label>
+                            <input type="number" className="cp__input i1" id="startyear2" aria-describedby="startyear" />
+                        </div>
+                        <div className="cp__field">
+                            <label htmlFor="endyear2" className="lblcp">End Year</label>
+                            <input type="text" className="cp__input i1" id="endyear2" />
+                        </div>
+                        </div>
+                        <div className="petticp-btn">
+                        <button type="button" className="delwkcp" id="deleteButton1" value="Delete">Delete</button>
+                        </div>
+                        <hr className="smhrr" />
+                    </div>
+                    <div className="petticp-btn" id="xxx32">
+                        <button type="button" className="addwkcp" id="addMoreButton2" value="addmore">Add More</button>
+                    </div>
+                    </div>
+                    <div id="formsavebtncp">
+                    <a className="savebtncp" href="#">
+                        <p>Save</p>
+                    </a>
+                    </div>
+                </div>
                 </form>
+                <div className="screen__backgroundcp">
+                <span className="screen__background__shapecp screen__background__shape4cp"></span>
+                <span className="screen__background__shapecp screen__background__shape3cp"></span>
+                <span className="screen__background__shapecp screen__background__shape2cp"></span>
+                <span className="screen__background__shapecp screen__background__shape1cp"></span>
+                </div>
             </section>
         </div>
-        </>
-    )
+    </div>
+    </>
+  );
 }
 
 export default Cand_profile
