c140e31871797a330f2b123a8c1d93f09eb4e8ce
NVNTH99 <navaneethshanavasan02@gmail.com>
2023-11-06 01:51:11 +0530

    env + client

--- client.js
+++ client.js
@@ -4,26 +4,26 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'React', 'dist')));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'React', 'dist','index.html'));
 });
 
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, 'React', 'dist','index.html'));
-});
+// app.get('/home', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'React', 'dist','index.html'));
+// });
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'React', 'dist','index.html'));
-});
+// app.get('/login', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'React', 'dist','index.html'));
+// });
 
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, 'React', 'dist','index.html'));
-  });
+// app.get('/signup', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'React', 'dist','index.html'));
+//   });
   
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'React', 'dist','index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'React', 'dist','index.html'));
+// });
 
 app.listen(5173, () => {
   console.log(`Server is running on port 5173`);
